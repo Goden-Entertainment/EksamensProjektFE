603662fed9a3d0aa3e212f3044b52ed7eefd22ef
Lornst <augustgosden@gmail.com>
2026-05-14 14:34:31 +0200

    changed prod backend api endpoint to match the production endpoint rather than the localhost one

--- js/bookingRequest.js
+++ js/bookingRequest.js
@@ -20,7 +20,7 @@ document.getElementById("bookingForm").addEventListener("submit", function (e) {
     };
     console.log("Sending:", JSON.stringify(bookingData));
     //Send to backend server.
-    fetch("http://localhost:8080/booking/create", {
+    fetch("http://20.251.162.251/api/booking/create", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
