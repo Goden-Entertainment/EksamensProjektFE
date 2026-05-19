a6e888b8378c62d92cc662fa13157f66bc7ba60c
mks <miso0007@stud.ek.dk>
2026-05-19 10:31:53 +0200

    show bookings created

--- js/admin.js
+++ js/admin.js
@@ -2,25 +2,28 @@ const API_URL = 'http://20.251.162.251/api/';
 const EMPTY_MESSAGE = 'Der er ingen anmodninger at vise i øjeblikket';
 
 async function fetchBookings() {
-    const res = await fetch(`${API_URL}/booking/all`);
+    const res = await fetch(`${API_URL}booking/all`);
     const bookings = await res.json();
     console.log(bookings);
 
     const list = document.getElementById('bookings-list');
+    list.innerHTML = '';
 
     if (bookings.length === 0) {
         list.innerHTML = `<p>${EMPTY_MESSAGE}</p>`;
     } else {
         bookings.forEach(booking => {
-            list.innerHTML += `
-        <div class="table-row">
+            const bookingRow = document.createElement('div');
+            bookingRow.classList.add('table-row');
+            bookingRow.innerHTML = `
             <div>${booking.startDate} - ${booking.endDate}</div>
             <div>${booking.companyName}</div>
             <div>${booking.addOns}</div>
             <div>${booking.guests}</div>
-            <div>${booking.bookingStatus}</div>
-        </div>
-    `;});
+            <div>${booking.bookingStatus}</div>`;
+
+            list.appendChild(bookingRow);
+        });
     }
 }
 
