0bc07f787c9166082a7eee967f50794687b3cfcb
Marco <m.stormly@yahoo.dk>
2026-05-20 12:03:57 +0200

    sorting efter tid

--- js/admin.js
+++ js/admin.js
@@ -4,6 +4,8 @@ const alleBtn = document.getElementById('btn-alle');
 const afventerBtn = document.getElementById('btn-afventer');
 const godkendtBtn = document.getElementById('btn-godkendt');
 const afvistBtn = document.getElementById('btn-afvist');
+const newestBtn = document.getElementById('btn-newest')
+const oldestBtn = document.getElementById('btn-oldest')
 const addOnsLabels = {
     OMF: "Overnatning med forplejning",
     OUF: "Overnatning uden forplejning",
@@ -26,6 +28,7 @@ async function fetchBookings() {
         const row = document.createElement('div');
         row.classList.add('table-row');
         row.dataset.status = booking.bookingStatus;
+        row.dataset.date = booking.startDate;
         row.innerHTML = `
             <div>${booking.startDate} - ${booking.endDate}</div>
             <div>${booking.companyName}</div>
@@ -33,6 +36,26 @@ async function fetchBookings() {
             <div>${booking.guests}</div>
             <div><span class="${statusClass(booking.bookingStatus)}">${translateStatus(booking.bookingStatus)}</span></div>
         `;
+        list.appendChild(row);
+    });
+}
+
+function sortBookings(order){
+    const list = document.getElementById('bookings-list')
+    const rows = Array.from(list.querySelectorAll('.table-row'));
+
+    rows.sort(function (a, b){
+        const dateA = new Date(a.dataset.date);
+        const dateB = new Date(b.dataset.date);
+
+        if(order === 'newest'){
+            return dateB - dateA;
+        }else {
+            return dateA - dateB;
+        }
+    });
+
+    rows.forEach(function (row){
         list.appendChild(row);
     });
 }
@@ -84,4 +107,10 @@ godkendtBtn.addEventListener('click', function() {
 
 afvistBtn.addEventListener('click', function() {
     filterBookings('REJECTED');
+});
+newestBtn.addEventListener('click', function (){
+    sortBookings('newest');
+});
+oldestBtn.addEventListener('click', function (){
+    sortBookings('oldest');
 });
